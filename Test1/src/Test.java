80030b4a577672687b3c22a1ebb934876ff5b1a5
Xingchao Long <z15881614642@163.com>
2020-01-26 23:04:04 +0800

    指导完成

---
+++ Test1/src/Test.java
@@ -0,0 +1,15 @@
+
+/**
+* @author Xingchao Long
+* @date 2020/31/26 18:31:05
+* @ClassName Test.java
+* @Description 类描述
+*/
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+
+}
