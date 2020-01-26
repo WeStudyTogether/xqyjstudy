ec09f675b8583620126c2e4b3aea8bedcb8fec98
prettyxqy <1792489051@qq.com>
2020-01-26 23:17:06 +0800

    java1026

---
+++ Variables/src/com/java21days/java211.java
@@ -0,0 +1,13 @@
+package com.java21days;
+
+public class java211 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    float count = 14000;
+    count = count + (count * .4F);
+    count = count - 1500F;
+    count = count + (count*.12F);   
+    System.out.println("After three years:$" + count);
+	}
+}
