a7fa8b0c4e3dd51add0526cf8ec60b699de9054f
prettyxqy <1792489051@qq.com>
2020-01-28 15:26:53 +0800

    1027

---
+++ Variables/src/com/java21days/sampler.java
@@ -0,0 +1,13 @@
+package com.java21days;
+
+public class sampler {
+  public static void main(String[] arguements) {
+	  sample thing = new sample();
+	  thing.height = 157;
+	  thing.weight = 50;
+	  thing.depth = 2;
+	  System.out.println("height: " + thing.height);
+	  System.out.println("weight: " + thing.weight);
+	  System.out.println("depth: " + thing.depth);
+  }
+} 
