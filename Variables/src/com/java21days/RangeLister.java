2afd36bab057053b48f5ffe6dfb5e3b43eb1b687
prettyxqy <1792489051@qq.com>
2020-01-31 02:18:32 +0800

    20200131

---
+++ Variables/src/com/java21days/RangeLister.java
@@ -0,0 +1,21 @@
+package com.java21days;
+
+public class RangeLister {
+  int[] makeRange(int lower,int upper) {
+	  int[] range = new int [(upper-lower) + 1];
+	  
+	  for (int i = 0; i < range.length; i++) {
+		  range[i] = lower++;
+	  }
+	  return range;
+  }
+  
+  public static void main(String[] arguments) {
+	  int[] range;
+	  RangeLister lister = new RangeLister();
+	  
+	  range = lister.makeRange(4, 13);
+	  System.out.print(range[i] + " ");
+      System.out.println(")");
+  }
+}
