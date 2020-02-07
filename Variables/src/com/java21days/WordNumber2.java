d1bf27a44a107df85b3bda979cca97df17bbda46
prettyxqy <1792489051@qq.com>
2020-02-08 00:40:27 +0800

    20200207

--- Variables/src/com/java21days/WordNumber2.java
+++ Variables/src/com/java21days/WordNumber2.java
@@ -1,11 +1,9 @@
 package com.java21days;
 //这个我不太懂。而且我用return会报错，因为他说void这是没有返回值的，所以我改了，总之就是不太明白
 public class WordNumber2 {
-	public static void main(String[] arguments) {
-		if (arguments.length > 0) {
-			  long num = 0;
-				  num = Integer.parseInt(arguements[0]);
-switch (num) {
+	public long oneToTen(String nums) {
+		long num = 0L;
+switch (nums) {
   case "one" :
 	  num = 1L;
       break;
@@ -36,7 +34,20 @@ switch (num) {
   case "ten":
 	  num = 10L;
 	  break;
-	  System.out.println("the number is :" + num);
-	  default:
-		  
+	 default:
+		 System.out.println("the default is :" + num);
+    }
+	return num;
 }
+	
+	public static void main(String[] arguments) {
+		WordNumber2 wordNumber2 = new WordNumber2();
+		if (arguments.length > 0) {
+			System.out.println(wordNumber2.oneToTen(arguments[0]));
+		}else {
+			System.out.println("请在控制台输入参数");
+		}
+		}
+		
+	}
+	
