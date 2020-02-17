a4cf6348abbda5fbbc7f868cfb43778e2b952030
Xingchao Long <z15881614642@163.com>
2020-02-17 20:59:25 +0800

    备注添加好了
    利用线程找第n个质数的算法

--- Variables/src/com/java21days/PrimeFinder2.java
+++ Variables/src/com/java21days/PrimeFinder2.java
@@ -1,40 +1,57 @@
 package com.java21days;
 
 public class PrimeFinder2  implements Runnable{
+    //第几个质数
 	public long target;
+	//找到的第target个质数
     public long prime;
+    //是否已经找到该质数
     public boolean finished = false;
+    //一个线程的变量
     private Thread runner;
 
+    //构造函数抛出NegativeNumberException
     PrimeFinder2(long inTarget) throws NegativeNumberException {
         if (inTarget < 0) {
+            //如果那个值小于0，就抛出这个自定义的异常
             NegativeNumberException nne = new NegativeNumberException(
                 "Negative numbers not allowed: " + inTarget);
             throw nne;
         }
         target = inTarget;
         if (runner == null) {
+            //新建线程
             runner = new Thread(this);
+            //启动线程
             runner.start();
         }
     }
  
+    //实现Runnable接口中的run方法，线程启动将会自动调用这个方法
     public void run() {
+        //用于计数
         long numPrimes = 0;
+        //最小的质数
         long candidate = 2;
+        //循环找到第n个质数
         while (numPrimes < target) {
+            //如果是质数，计数的变量就加一
             if (isPrime(candidate)) {
                 numPrimes++;
                 prime = candidate;
             }
             candidate++;
         }
+        //找到之后，将finish设置为true，以表示这个线程已经结束
         finished = true;
     }
 
+    //用于判断是不是质数的函数，质数——只能被1和其本身整除的数
     boolean isPrime(long checkNumber) {
+        //对数值开平方根，优化这个查找算法，原本应该是i <= checkNumber，为什么能这样做，参考下述链接：https://zhidao.baidu.com/question/50140553.html
         double root = Math.sqrt(checkNumber); 
         for (int i = 2; i <= root; i++) {
+            //能被整除，就返回false，表明不是质数
             if (checkNumber % i == 0) {
                 return false;
             }
@@ -44,12 +61,16 @@ public class PrimeFinder2  implements Runnable{
 }
 
 @SuppressWarnings("serial")
+//内部类——在一个类的内部能再定义一个类，自定义一个NegativeNumberException的异常了类
 class NegativeNumberException extends Exception {
+    //无参数的构造函数
     NegativeNumberException() {
+        //调用超类的构造函数
         super();
     }
-
+    //有参数构造函数
     NegativeNumberException(String message) {
+        //调用超类的构造函数
         super(message);
     }
 }
--- Variables/src/com/java21days/PrimeThreads2.java
+++ Variables/src/com/java21days/PrimeThreads2.java
@@ -3,6 +3,7 @@ package com.java21days;
 public class PrimeThreads2 {
     public static void main(String[] arguments) {
         try {
+            //下面这一段代码是在初始化finder数组对象，值从控制台中录入
             PrimeFinder2[] finder = new PrimeFinder2[arguments.length];
             for (int i = 0; i < arguments.length; i++) {
                 long count = Long.parseLong(arguments[i]);
@@ -10,6 +11,12 @@ public class PrimeThreads2 {
                 System.out.println("Looking for prime " + count);
             }
             boolean complete = false;
+            /**
+             * 下面这个循环用来保证finder这个数组中的所有对象都已经完成要做的事
+             * 完成要做的事之后，会将finder中的finished变量设置为true，没调用一次构造函数就会开启一个线程，然后无规律的交叉执行
+             * 下面这个循环的意思就是有数组中的对象有一个没有完成，就将complete重置为false继续等待，直到所有的线程都结束
+             * 从而保证每个线程结束之后，主线程才接着往下进行。
+             */
             while (!complete) {
                 complete = true;
                 for (int j = 0; j < finder.length; j++) {
@@ -17,18 +24,22 @@ public class PrimeThreads2 {
                         complete = false;
                 }    
                 try { 
+                    //每判断一次等待一秒的时间休眠，这个时间给数组中的每一个对象做他们自己的事
                     Thread.sleep(1000);
                 } catch (InterruptedException ie) {
                     // do nothing
                 }
             }
+            //打印出结果，即第target个质数是多少
             for (int j = 0; j < finder.length; j++) {
                 System.out.println("Prime " + finder[j].target
                     + " is " + finder[j].prime);
             }    
         } catch (NumberFormatException nfe) {
                 System.out.println("Error: " + nfe.getMessage());
-        } catch (NegativeNumberException nne) {
+        } 
+        //添加下面这个catch语句，用来处理NegativeNumberException的自定义异常类
+        catch (NegativeNumberException nne) {
                 System.out.println("Error: " + nne.getMessage());
         }
     }
